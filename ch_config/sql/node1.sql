bb492131a920b871f8ec490d27a28085bd1b2aff
Shuichi <zhuravkov.mai@gmail.com>
2023-05-17 17:11:06 +0300

    updated schema

--- ch_config/sql/node1.sql
+++ ch_config/sql/node1.sql
@@ -4,23 +4,26 @@ CREATE DATABASE IF NOT EXISTS replica;
 CREATE TABLE IF NOT EXISTS shard.view(
     user_id String,
     film_id String,
-    frame Int16,
-    event_time DateTime
+    start_time UInt16,
+    end_time UInt16,
+    event_time DateTime DEFAULT now()
 )
 Engine=ReplicatedMergeTree('/clickhouse/tables/shard1/view', 'replica_1') PARTITION BY toYYYYMMDD(event_time) ORDER BY event_time;
 
 CREATE TABLE IF NOT EXISTS replica.view(
     user_id String,
     film_id String,
-    frame Int16,
-    event_time DateTime
+    start_time UInt16,
+    end_time UInt16,
+    event_time DateTime DEFAULT now()
 )
 Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/view', 'replica_2') PARTITION BY toYYYYMMDD(event_time) ORDER BY event_time;
 
 CREATE TABLE IF NOT EXISTS default.view(
     user_id String,
     film_id String,
-    frame Int16,
-    event_time DateTime
+    start_time UInt16,
+    end_time UInt16,
+    event_time DateTime DEFAULT now()
 )
 Engine=Distributed('company_cluster', '', view, rand());
