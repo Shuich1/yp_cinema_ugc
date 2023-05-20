1b45e9a70796620cd285625ca68976cadd907a00
Kostyuchenko Anton <nghst@ngs.ru>
2023-05-20 14:03:59 +0000

    123

--- ugc_service/src/db/olap.py
+++ ugc_service/src/db/olap.py
@@ -1,5 +1,8 @@
 from typing import Optional
 from abc import ABC, abstractmethod
+from core.config import settings
+from asynch import connect
+import backoff
 
 
 class GenericOlap(ABC):
@@ -8,6 +11,31 @@ class GenericOlap(ABC):
     @abstractmethod
     async def disconnect(self):
         pass
+
+
+class ClickHouseOlap(GenericOlap):
+    def __init__(self, bootstrap_servers: list) -> None:
+        self.bootstrap_servers = bootstrap_servers
+
+    @backoff.on_exception(backoff.expo, ConnectionRefusedError, max_time=settings.BACKOFF_MAX_TIME)   
+    async def connect(self) -> None:
+        self.connection = await connect(
+            host=settings.CLICKHOUSE_HOST,
+            port=settings.CLICKHOSE_PORT,
+            database=settings.CLICKHOUSE_DB,
+            # user = "default",
+            # password = "",
+        )
+
+    async def _execute(self, query) -> str:
+        async with self.connection.cursor() as cursor:
+            await cursor.execute(query)
+            return cursor
+    
+    async def disconnect(self) -> None:
+        await self.producer.stop()
+
+
 
 
 olap_bd: Optional[GenericOlap] = None
