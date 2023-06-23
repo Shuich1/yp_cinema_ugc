eef80973a851530b38a6055fd3e9f7fbb05fb8c0
Georgii Andreev <andreev.g@live.com>
2023-06-23 18:11:00 +0300

    test: Implement reviews api tests

--- ugc_service/tests/functional/src/api/reviews/test_delete_review_vote.py
+++ ugc_service/tests/functional/src/api/reviews/test_delete_review_vote.py
@@ -1,28 +1,91 @@
+from http import HTTPStatus
+from uuid import uuid4
+
 import pytest
+
+from testdata import Review, ReviewVote
+from utils import write_to_db, extract_from_db
 
 endpoint_url = '/reviews/{review_id}/votes/{user_id}'
 endpoint_method = 'delete'
 
 
-def test_correct_authenticated_request_deletes_a_vote_with_code_204():
-    pytest.fail()
+def test_correct_authenticated_request_deletes_a_vote_with_code_204(
+        db,
+        api_request,
+):
+    review = Review()
+    write_to_db(db.reviews, review)
+
+    user_id = uuid4()
+    review_vote = ReviewVote(review_id=review.review_id, user_id=user_id)
+    write_to_db(db.review_votes, review_vote)
+
+    response = api_request(
+        endpoint_method,
+        endpoint_url.format(review_id=review_vote.review_id, user_id=user_id),
+        user_id=user_id,
+    )
+
+    assert response.status_code == HTTPStatus.NO_CONTENT
 
 
-def test_vote_deletion_is_reflected_in_a_corresponding_review():
-    pytest.fail()
+def test_unauthenticated_request_results_in_error_401(api_request):
+    response = api_request(
+        endpoint_method,
+        endpoint_url.format(review_id=uuid4(), user_id=uuid4()),
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
 
 
-def test_unauthenticated_request_results_in_error_401():
-    pytest.fail()
+def test_unauthorized_request_results_in_error_403(
+        db,
+        api_request,
+):
+    review = Review()
+    write_to_db(db.reviews, review)
+
+    user_id = uuid4()
+    review_vote = ReviewVote(review_id=review.review_id, user_id=user_id)
+    write_to_db(db.review_votes, review_vote)
+
+    other_user_id = uuid4()
+
+    response = api_request(
+        endpoint_method,
+        endpoint_url.format(review_id=review_vote.review_id, user_id=user_id),
+        user_id=other_user_id,
+    )
+
+    assert response.status_code == HTTPStatus.FORBIDDEN
 
 
-def test_unauthorized_request_results_in_error_403():
-    pytest.fail()
+def test_request_for_a_nonexistent_vote_results_in_error_404(api_request):
+    user_id = uuid4()
+
+    response = api_request(
+        endpoint_method,
+        endpoint_url.format(review_id=uuid4(), user_id=user_id),
+        user_id=user_id,
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_request_for_a_nonexistent_vote_results_in_error_404():
-    pytest.fail()
+@pytest.mark.parametrize(
+    ['review_id', 'user_id'],
+    [(uuid4(), 'invalid'), ('invalid', uuid4())],
+)
+def test_request_for_invalid_vote_id_results_in_error_422(
+        api_request,
+        review_id,
+        user_id,
+):
+    response = api_request(
+        endpoint_method,
+        endpoint_url.format(review_id=review_id, user_id=user_id),
+        user_id=user_id,
+    )
 
-
-def test_request_for_invalid_vote_id_results_in_error_422():
-    pytest.fail()
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
--- ugc_service/tests/functional/testdata.py
+++ ugc_service/tests/functional/testdata.py
@@ -1,8 +1,12 @@
 from datetime import datetime
-from random import randint
+from random import randint, choice
+from typing import Literal
 from uuid import UUID, uuid4
 
+from faker import Faker
 from pydantic import BaseModel, Field
+
+fake = Faker()
 
 
 class Bookmark(BaseModel):
@@ -25,3 +29,23 @@ class Rating(BaseModel):
     @property
     def id(self) -> tuple[str, str]:
         return str(self.film_id), str(self.user_id)
+
+
+class Review(BaseModel):
+    review_id: UUID = Field(default_factory=uuid4)
+    film_id: UUID = Field(default_factory=uuid4)
+    user_id: UUID = Field(default_factory=uuid4)
+    body: str = Field(default_factory=fake.text)
+    created: datetime = Field(default_factory=fake.date_time_this_year)
+    likes: int = 0
+    dislikes: int = 0
+
+
+class ReviewVote(BaseModel):
+    review_id: UUID = Field(default_factory=uuid4)
+    user_id: UUID = Field(default_factory=uuid4)
+    vote: str = Field(default_factory=lambda: choice(('like', 'dislike')))
+
+    @property
+    def id(self) -> tuple[str, str]:
+        return str(self.review_id), str(self.user_id)
